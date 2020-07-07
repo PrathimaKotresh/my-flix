776d8a373608e466b30a6da9d0345d38487cb461
Prathima Kotresh <prathima.kotresh@gmail.com>
2020-07-07 09:36:09 +0200

    added dependencies and comments to hash password

--- index.js
+++ index.js
@@ -79,27 +79,25 @@ app.get('/movies/director/:name', (req, res) => {
 
 // Creates a new user
 app.post('/users', (req, res) => {
-  Users.findOne({
-      Username: req.body.Username
-    })
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
     .then((user) => {
       if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
       } else {
         Users
           .create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
-          .then((user) => {
-            res.status(201).json(user)
-          })
+          .then((user) => { res.status(201).json(user) })
           .catch((error) => {
             console.error(error);
             res.status(500).send('Error: ' + error);
-          })
+          });
       }
     })
     .catch((error) => {
@@ -107,6 +105,7 @@ app.post('/users', (req, res) => {
       res.status(500).send('Error: ' + error);
     });
 });
+
 
 // Updates the user data
 app.put('/users/:Username', (req, res) => {
