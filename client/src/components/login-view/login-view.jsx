2ce672ba2a94e4fe50722704bdd4c4685857bd3f
Prathima Kotresh <prathima.kotresh@gmail.com>
2020-07-30 16:49:52 +0200

    enable jwt authentication for movies endpoint

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
@@ -10,9 +11,18 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    // Send a request to the server for authentication then call props.onLoggedIn(username)
-    props.onLoggedIn(username);
+    /* Send a request to the server for authentication */
+    axios.post('YOUR_API_URL/login', {
+      Username: username,
+      Password: password
+    })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
   };
 
   return (
@@ -24,7 +34,7 @@ export function LoginView(props) {
             <Form.Control type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label>Password:</Form.Label>
             <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
           </Form.Group>
           <Button variant="primary" type="submit" onClick={handleSubmit}>
