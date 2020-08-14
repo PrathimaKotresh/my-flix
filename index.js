7ba624718dc40f3e5a1328693219133e4c2885d3
Prathima Kotresh <prathima.kotresh@gmail.com>
2020-08-15 00:59:16 +0200

    code reordering

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const cors = require('cors');
 const express = require('express'),
   bodyParser = require('body-parser'),
@@ -10,6 +9,10 @@ const app = express();
 app.use(bodyParser.json());
 const passport = require('passport');
 require('./passport.js');
+
+const path = require('path');
+
+var port = process.env.PORT || 8080;
 
 //require express-validator library
 const {
@@ -35,6 +38,17 @@ mongoose.connect(process.env.CONNECTION_URI, {
 });
 
 app.use(morgan('common'));
+
+// routes all requests for static files to 'public' folder
+app.use(express.static('public'));
+
+// routes all requests for the client to 'dist' folder
+app.use('/client', express.static(path.join(__dirname, 'client/dist')));
+// all routes to the React client
+app.get('/client/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
+
 var allowedOrigins = ['http://localhost:1234', '*'];
 
 app.use(cors({
@@ -272,16 +286,9 @@ app.delete('/users/:Username', (req, res) => {
     });
 });
 
-// listen for requests
-const port = process.env.PORT || 8080;
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
-});
-app.use(express.static("public"));
-app.use("/client", express.static(path.join(__dirname, "client", "dist")));
-app.get("/client/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
